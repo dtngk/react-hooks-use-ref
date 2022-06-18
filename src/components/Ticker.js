c16a4a6093950e4af2fa21dd1bc4ea8544cf6136
Dat Nguyen <dnguyenk2@gmail.com>
2022-06-18 12:13:50 -0700

    Completed Assignment

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,13 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { makeRandomNumber } from "../utils";
 
 function Ticker() {
   const [price, setPrice] = useState(0);
   const [color, setColor] = useState("black");
+  // create the ref and set the initial value
+  const prevPriceRef = useRef(price);
+
+
+  useEffect(() => {
+    const prevPrice = prevPriceRef.current;
+    
+    if (price > prevPrice){
+      setColor("green");
+    } else if (price < prevPrice){
+      setColor("red");
+    } else{
+      setColor("black");
+    }
+
+    // set the new value of the ref (note: this doesn't trigger a re-render)
+    prevPriceRef.current = price;
+
+  }, [price]);
 
   useEffect(() => {
     const id = setInterval(() => setPrice(makeRandomNumber), 1000);
-    return function () {
+    return function cleanup() {
       clearInterval(id);
     };
   }, []);
